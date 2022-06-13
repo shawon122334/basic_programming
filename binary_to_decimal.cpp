227868e98f15ae1e5437529e1cd20699b3144d76
sabbir <shawon.mu11@gmail.com>
2022-06-14 02:43:19 +0600

    binary to decimal

--- binary_to_decimal.cpp
+++ binary_to_decimal.cpp
@@ -20,4 +20,7 @@ int main(){
 /*
     we can not take binary input, computer always treat inputs as an decimal. then it converts them into binary
     if we take 101 as input, computer treats it as decimal and converts into 1100101. but we dont want this, so we take the reminder(digit) and if it is 1 then execute the formula.
+
+    if digit 1 
+        2^3 + 2^2 + 2^1 + 2^0 = something
 */
